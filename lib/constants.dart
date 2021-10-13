13c7cc577a351b7849fe3c2cbd0b1e79a2ba0426
PranavNair01 <n.pranav@iitg.ac.in>
2021-10-13 19:44:05 +0530

    Phone Authentication, and SharedPreferences without CloudFirestore fixed

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,4 +5,5 @@ import 'package:location_tracking_2/main.dart';
 import 'package:location_tracking_2/models/user.dart';
 
 // Current logged in user (by default null)
+var kMobileNumber;
 User kCurrUser;
